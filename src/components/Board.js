7a681462d4fca5c7414c6479dd8e4d2b477ff2da
venkatesh kumar <venkatesh.k@ampcome.com>
2022-04-14 02:13:14 +0530

    Added Events

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import Square from "./Square";
 
+const values = ["", "", "", "", "", "", "", "", ""];
+
 const Board = () => {
+  const [state, setState] = useState(values);
+  const [isX, setIsX] = useState(false);
+
+  const handleTurns = (e) => {
+    navigator.vibrate(30);
+    let temp = [...state];
+    temp[e] = isX ? "X" : "O";
+    setState(temp);
+    setIsX(!isX);
+  };
+
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
       <div className="d-flex">
-        <Square border={"border-b-r"} />
-        <Square border={"border-b"} />
-        <Square border={"border-b-l"} />
+        <Square
+          border={"border-b-r"}
+          value={state[0]}
+          event={() => handleTurns(0)}
+        />
+        <Square
+          border={"border-b"}
+          value={state[1]}
+          event={() => handleTurns(1)}
+        />
+        <Square
+          border={"border-b-l"}
+          value={state[2]}
+          event={() => handleTurns(2)}
+        />
       </div>
       <div className="d-flex">
-        <Square border={"border-b-r"} />
-        <Square border={"border-b"} />
-        <Square border={"border-b-l"} />
+        <Square
+          border={"border-b-r"}
+          value={state[3]}
+          event={() => handleTurns(3)}
+        />
+        <Square
+          border={"border-b"}
+          value={state[4]}
+          event={() => handleTurns(4)}
+        />
+        <Square
+          border={"border-b-l"}
+          value={state[5]}
+          event={() => handleTurns(5)}
+        />
       </div>
       <div className="d-flex">
-        <Square border={"border-r"} />
-        <Square />
-        <Square border={"border-l"} />
+        <Square
+          border={"border-r"}
+          value={state[6]}
+          event={() => handleTurns(6)}
+        />
+        <Square value={state[7]} event={() => handleTurns(7)} />
+        <Square
+          border={"border-l"}
+          value={state[8]}
+          event={() => handleTurns(8)}
+        />
       </div>
     </div>
   );
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./components.css";
 
-const Square = ({ border }) => {
+const Square = ({ border, value, event }) => {
   return (
     <div
       className={`item p-4 ${border}`}
       style={{ height: "100px", width: "100px", borderRadius: 2 }}
+      onClick={event}
     >
-      <h1 className="text-light">X</h1>
+      <h1 className="text-light">{value}</h1>
     </div>
   );
 };
