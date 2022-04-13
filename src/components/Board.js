b653bcaa81c49a9e1c5e4a9fab6f8fe7cc358c87
venkatesh kumar <venkatesh.k@ampcome.com>
2022-04-14 03:22:29 +0530

    Added Winner Logic

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Square from "./Square";
 
 const values = ["", "", "", "", "", "", "", "", ""];
@@ -13,6 +13,34 @@ const Board = () => {
     temp[e] = isX ? "X" : "O";
     setState(temp);
     setIsX(!isX);
+  };
+
+  useEffect(() => {
+    const result = handleWinner();
+    if (result) {
+      alert(result + " won the game.....!!!1");
+      setState(values);
+    }
+  }, [state]);
+
+  const handleWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (state[a] && state[a] === state[b] && state[a] === state[c]) {
+        return state[a];
+      }
+    }
+    return null;
   };
 
   return (
